0cafe4474755614ca1f1118c4425dddd8d170d01
Saumya Singh <saumyasingh83896>
2026-09-03 11:23:19 +0530

    arrow function

---
+++ Day_6/arrow_function.js
@@ -0,0 +1,12 @@
+// Normal arrow function
+const fullName = (name, lastname) => {
+    return `${name} ${lastname}`;
+};
+
+console.log(fullName("Saumya", "Singh"));
+
+
+// Short arrow function
+// const fullName = (name, lastname) => `${name} ${lastname}`;
+
+// console.log(fullName("Saumya", "Singh"));
