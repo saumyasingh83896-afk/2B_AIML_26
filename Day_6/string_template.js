29c2f5f7cf60142c466bfcfd4015df1e84fecd5e
Saumya Singh <saumyasingh83896>
2026-09-03 11:08:37 +0530

    string template practice

---
+++ Day_6/string_template.js
@@ -0,0 +1,20 @@
+ let name = "Saumya";
+ let lastname = "Singh";
+ let age = 20;
+// let message = "My name is " + name + " " + lastname + " and I am " + age + " years old.";
+// console.log(message);
+
+
+//let message = `My name is ${name} ${lastname} and I am ${age} years old.`;
+
+//console.log(message);
+
+function fullName(name, lastname) {
+    let fullname = `${name} ${lastname}`;
+    console.log(fullname);
+}
+
+fullName("Saumya", "Singh");
+
+let Name = `Hello, ${name} ${lastname} and I am ${age} years old.`;
+console.log(Name);
