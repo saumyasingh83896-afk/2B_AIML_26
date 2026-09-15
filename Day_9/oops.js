4fa94c6bb377cbdf7765701b8120d4fc2fe783eb
Saumya Singh <saumyasingh83896>
2026-09-15 11:54:53 +0530

    oops

---
+++ Day_9/oops.js
@@ -0,0 +1,14 @@
+
+   class student {
+ /*   constructor() {
+        console.log("Constructor called");
+    }*/
+
+constructor(name) {
+    this.name = name;
+    console.log(name);
+}
+}
+
+let s=new student("Saumya");
+console.log(s.name);
